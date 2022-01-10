8dbe2cd0a566c2469b8047ca4a45ba5d08835670
Darya Iakovleva <888darya@gmail.com>
2022-01-10 16:29:51 +0300

    Декоративные элементы внутренних страниц

--- source/js/filter.js
+++ source/js/filter.js
@@ -1,32 +1,11 @@
-const letters = document.querySelector(".letters");
-const buttons = letters.querySelectorAll(".letters__button");
-const countries = document.querySelector('.countries');
-const countriesSections = countries.querySelectorAll('.countries__wrapper');
+const filter = document.querySelector('.filter');
+const filterButtons = filter.querySelectorAll('.filter__button');
 
-function changeCountriesSection(letter) {
-  countriesSections.forEach(section => {
-    const sectionLetter = section.querySelector('.countries__letter').textContent;
-
-    if (letter === sectionLetter) {
-      const activeSection = countries.querySelector('.countries__wrapper--active');
-      activeSection.classList.remove('countries__wrapper--active');
-      section.classList.add('countries__wrapper--active');
-    }
-  });
-}
-
-function buttonClickHandler(e) {
+function buttonsClickHandler(e) {
   e.preventDefault();
-  const activeButton = letters.querySelector(".letters__button--active");
-  const letter = this.textContent;
-  if (activeButton) {
-    activeButton.classList.remove("letters__button--active");
-  }
-  this.classList.add("letters__button--active");
-  changeCountriesSection(letter)
-
+  this.classList.toggle('filter__button--active');
 }
 
-buttons.forEach((button) => {
-  button.addEventListener("click", buttonClickHandler);
+filterButtons.forEach((button) => {
+  button.addEventListener('click', buttonsClickHandler);
 });
